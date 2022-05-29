7aea8732046ee01664309c5dc934d5b5c30fcbbb
vlada <aleks_ada@mai.ru>
2022-05-29 16:10:38 +0300

    Solved task6

--- Task6.py
+++ Task6.py
@@ -6,12 +6,13 @@ matrix = [[11, 2, 5],
 
 count = 0
 even = 0
-max_column = 0
-min_column = 0
+max_column_index = 0
+min_column_index = 0
 max_even = 0
 min_even = 0
-changed_matrix = matrix
-#скорее всего обнуляется каждый проход значение min_column и max_column, дебажить
+changed_matrix = [[0, 0, 0], [0, 0, 0]]
+
+# скорее всего обнуляется каждый проход значение min_column и max_column, дебажить
 # поиск минимального и максимального колчества четных элементов по столбцам
 
 for i in range(3):
@@ -20,19 +21,22 @@ for i in range(3):
             even += 1
         if even > max_even:
             max_even = even
-            max_column = i
+            max_column_index = i
         if even < min_even:
             min_even = even
-            min_column = i
+            min_column_index = i
     even = 0
 
 # смена столбцов местами
 for i in range(3):
     for j in range(2):
-        if i == min_column:
-            changed_matrix[i][j] = matrix[min_column][j]
-        if i == max_column:
-            changed_matrix[i][j] = matrix[max_column][j]
+        if i == min_column_index:
+            changed_matrix[j][i] = matrix[j][max_column_index]
+        elif i == max_column_index:
+            changed_matrix[j][i] = matrix[j][min_column_index]
+        elif i != min_column_index & i != max_column_index:
+            changed_matrix[j][i] = matrix[j][i]
+
 
 print(matrix)
 print(changed_matrix)
